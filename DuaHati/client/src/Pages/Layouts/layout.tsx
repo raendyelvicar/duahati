8b2beeb19ee7ae16d817b06d3fda080cb8bfa84e
raendy.elvicar@gmail.com <raendy.elvicar@gmail.com>
2024-12-08 01:12:33 +0700

    setup component

--- DuaHati/client/src/Pages/Layouts/layout.tsx
+++ DuaHati/client/src/Pages/Layouts/layout.tsx
@@ -1,14 +1,142 @@
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/app-sidebar"
+import Dropdown from "@/components/ui/dropdown";
+import { PropsWithChildren, ReactNode, useState } from "react";
+import ResponsiveNavLink from "@/components/ui/responsive-nav-link";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({ 
+    header, 
+    children 
+}: PropsWithChildren<{ header?: ReactNode }>) {
+    const [showingNavigationDropdown, setShowingNavigationDropdown] =
+        useState(false);
     return (
         <SidebarProvider>
             <AppSidebar />
-            <main>
-                <SidebarTrigger />
-                {children}
-            </main>
+            <SidebarInset>  
+                <div className="min-h-screen bg-gray-100">
+                    <nav className="border-b border-gray-100 bg-white">
+                        <div className="mx-auto w-full px-4 sm:px-6 lg:px-8">
+                            <div className="flex h-16 items-center justify-between">
+                                <SidebarTrigger />
+                                
+                                <div className="hidden sm:ms-6 sm:flex sm:items-center">
+                                    <div className="relative ms-3">
+                                        <Dropdown>
+                                            <Dropdown.Trigger>
+                                                <span className="inline-flex rounded-md">
+                                                    <button
+                                                        type="button"
+                                                        className="inline-flex items-center rounded-md border border-transparent bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none"
+                                                    >
+                                                        username
+
+                                                        <svg
+                                                            className="-me-0.5 ms-2 h-4 w-4"
+                                                            xmlns="http://www.w3.org/2000/svg"
+                                                            viewBox="0 0 20 20"
+                                                            fill="currentColor"
+                                                        >
+                                                            <path
+                                                                fillRule="evenodd"
+                                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                                clipRule="evenodd"
+                                                            />
+                                                        </svg>
+                                                    </button>
+                                                </span>
+                                            </Dropdown.Trigger>
+
+                                            <Dropdown.Content>
+                                                <Dropdown.Link
+                                                   to={""}
+                                                >
+                                                    Profile
+                                                </Dropdown.Link>
+                                                <Dropdown.Link
+                                                    to={""}
+                                                >
+                                                    Log Out
+                                                </Dropdown.Link>
+                                            </Dropdown.Content>
+                                        </Dropdown>
+                                    </div>
+                                </div>
+
+                                <div className="me-2 flex items-center sm:hidden">
+                                    <button
+                                        onClick={() =>
+                                            setShowingNavigationDropdown(
+                                                (previousState) => !previousState,
+                                            )
+                                        }
+                                        className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 transition duration-150 ease-in-out hover:bg-gray-100 hover:text-gray-500 focus:bg-gray-100 focus:text-gray-500 focus:outline-none"
+                                    >
+                                        <svg
+                                            className="h-6 w-6"
+                                            stroke="currentColor"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                        >
+                                            <path
+                                                className={
+                                                    !showingNavigationDropdown
+                                                        ? 'inline-flex'
+                                                        : 'hidden'
+                                                }
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                strokeWidth="2"
+                                                d="M4 6h16M4 12h16M4 18h16"
+                                            />
+                                            <path
+                                                className={
+                                                    showingNavigationDropdown
+                                                        ? 'inline-flex'
+                                                        : 'hidden'
+                                                }
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                strokeWidth="2"
+                                                d="M6 18L18 6M6 6l12 12"
+                                            />
+                                        </svg>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div
+                            className={
+                                (showingNavigationDropdown ? 'block' : 'hidden') +
+                                ' sm:hidden'
+                            }
+                        >
+
+                            <div className="border-t border-gray-200 pb-1 pt-4">
+                                <div className="px-4">
+                                    <div className="text-base font-medium text-gray-800">
+                                        Username
+                                    </div>
+                                    <div className="text-sm font-medium text-gray-500">
+                                        Email
+                                    </div>
+                                </div>
+
+                                <div className="mt-3 space-y-1">
+                                    <ResponsiveNavLink to={""} >
+                                        Profile
+                                    </ResponsiveNavLink>
+                                    <ResponsiveNavLink to={""}                                    >
+                                        Log Out
+                                    </ResponsiveNavLink>
+                                </div>
+                            </div>
+                        </div>
+                    </nav>
+                    <main>{children}</main>
+                </div>
+            </SidebarInset>
         </SidebarProvider>
     )
 }
--- DuaHati/client/src/Pages/Mempelai/Index.tsx
+++ DuaHati/client/src/Pages/Mempelai/Index.tsx
@@ -1,9 +1,29 @@
+import { Button } from "@/components/ui/button";
 import Layout from "../Layouts/Layout";
+import { Link } from "react-router-dom";
+import { ChevronLeft } from "lucide-react";
 
 export default function Index() {
     return (
-        <Layout>
-            Index Page
+        <Layout
+            header={
+                <h2 className="text-xl font-semibold leading-tight text-gray-800">
+                    Profil Mempelai
+                </h2>
+            }
+        >
+            <div className="py-12">
+                <div className="mx-auto max-w-7xl space-y-6 sm:px-6 lg:px-8">
+                    <Button variant="outline" size="icon" asChild>
+                        <Link to={""} >
+                            <ChevronLeft />
+                        </Link>
+                    </Button>
+                    <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8">
+                        
+                    </div>
+                </div>
+            </div>
         </Layout>
     )
 }
